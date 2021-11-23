9f5182cb50f76612407016feb633e2f32ea406a0
Courtney Long <courtneylong877@gmail.com>
2021-11-22 22:50:51 -0500

    Added models for category, product, and tag

--- models/Product.js
+++ models/Product.js
@@ -9,7 +9,24 @@ class Product extends Model {}
 // set up fields and rules for Product model
 Product.init(
   {
-    // define columns
+    product_id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    product_name: {
+      type: DataTypes.STRING,
+    },
+    price: {
+      type: DataTypes.INTEGER,
+    },
+    stock: {
+      type: DataTypes.INTEGER,
+    },
+    category_id: {
+      type: DataTypes.INTEGER,
+      secondaryKey: true
+    },
   },
   {
     sequelize,
