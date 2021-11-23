e1773e4025838b194793b57fe23b80bfc7d32783
Courtney Long <courtneylong877@gmail.com>
2021-11-23 00:11:55 -0500

    Added belongs to

--- models/index.js
+++ models/index.js
@@ -6,11 +6,28 @@ const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
 
+Product.belongsTo(Category, {
+  foreignKey: "category_id",
+  onDelete: "Cascade"
+});
+
 // Categories have many Products
 
 // Products belongToMany Tags (through ProductTag)
 
+Product.belongsTo(Tag, {
+  through: "ProductTag",
+  foreignKey: "tag_id",
+  onDelete: "Cascade"
+});
+
 // Tags belongToMany Products (through ProductTag)
+
+Tag.belongsTo(Tag, {
+  through: "ProductTag",
+  foreignKey: "product_id",
+  onDelete: "Cascade"
+});
 
 module.exports = {
   Product,
